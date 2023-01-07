9f3c40c1966bf197126a89d141709a1d813bd334
eunji <dmswl6310@gmail.com>
2023-01-08 00:58:51 +0900

    Initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `주니어 개발자의 개발 블로그`,
+    description: `주니어 개발자로서의 저를 표현한 블로그입니다.`,
+    author: `Eunji`,
+    siteUrl:'https://dmswl6310.github.io/'
   },
   plugins: [
+    {
+resolve:'gatsby-plugin-canonical-urls',
+options:{
+  siteUrl:'https://dmswl6310.github.io/',
+  stripQueryString:true,
+}
+    },
     {
       resolve: 'gatsby-plugin-typescript',
       options: {
