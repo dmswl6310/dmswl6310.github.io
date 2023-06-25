fdc0e3114c9c6d090e04873e2c8d7f958c29b6e9
eunji <dmswl6310@gmail.com>
2023-06-25 16:54:48 +0900

    이미지 lazy loading  적용

--- src/components/Main/ProfileImage.tsx
+++ src/components/Main/ProfileImage.tsx
@@ -1,11 +1,16 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
+import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image'
 
 // 자신이 원하는 프로필 이미지 링크로 설정해주세요.
-const PROFILE_IMAGE_LINK =
-  'https://avatars.githubusercontent.com/u/45475492?s=400&u=c5843124c94a24eb3651fbc5d54e38dfbbe75818&v=4'
+// const PROFILE_IMAGE_LINK =
+//   'https://avatars.githubusercontent.com/u/45475492?s=400&u=c5843124c94a24eb3651fbc5d54e38dfbbe75818&v=4'
 
-const ProfileImageWrapper = styled.img`
+type ProfileImageProps = {
+  profileImage: IGatsbyImageData
+}
+
+const ProfileImageWrapper = styled(GatsbyImage)`
   width: 120px;
   height: 120px;
   margin-bottom: 30px;
@@ -18,8 +23,10 @@ const ProfileImageWrapper = styled.img`
   }
 `
 
-const ProfileImage: FunctionComponent = function () {
-  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+const ProfileImage: FunctionComponent<ProfileImageProps> = function ({
+  profileImage,
+}) {
+  return <ProfileImageWrapper image={profileImage} alt="Profile Image" />
 }
 
 export default ProfileImage
