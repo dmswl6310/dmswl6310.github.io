34179279ce361a9d2785f7abdf06e5d7dbcd062f
eunji <ejhwang@seegene.com>
2023-09-11 19:35:42 +0900

    파비콘 추가

--- src/components/Common/Template.tsx
+++ src/components/Common/Template.tsx
@@ -28,36 +28,53 @@ const Template: FunctionComponent<TemplateProps> = function ({
     <Container>
       <Helmet>
         <title>{title}</title>
-
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
-
         <meta property="og:type" content="website" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={image} />
         <meta property="og:url" content={url} />
         <meta property="og:site_name" content={title} />
-
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={image} />
         <meta name="twitter:site" content="dmswl6310" />
         <meta name="twitter:creator" content="dmswl6310" />
-
         <meta
           name="google-site-verification"
           content="FGJE3aR3D5ODy6mkH4NNCl-MlsMr5WdouofKYdZrDf0"
         />
-
         <meta
           name="naver-site-verification"
           content="27658bef328aa68e9eb5eecfce9c4a0e7cc24994"
         />
-
         <html lang="ko" />
+        // 파비콘 추가
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        // 파비콘 추가
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Helmet>
 
       <GlobalStyle />
