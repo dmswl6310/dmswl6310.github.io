bfcea074e6c2870793fef8eab4c09d7fdf1df959
eunji <ejhwang@seegene.com>
2023-09-11 21:00:49 +0900

    feat: make side navigation. (not completed)

---
+++ src/components/Common/SideNavi.tsx
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled'
+import { FunctionComponent } from 'react'
+
+const SideNavi: FunctionComponent = function () {
+  return (
+    // <NaviWrapper>
+    //   {Object.entries(categoryList).map(([name, count]) => (
+    //     <CategoryItem
+    //       to={`/?category=${name}`}
+    //       active={name === selectedCategory}
+    //       key={name}
+    //     >
+    //       #{name}({count})
+    //     </CategoryItem>
+    //   ))}
+    // </NaviWrapper>
+  )
+}
+
+const NaviWrapper = styled.nav``
+
+export default SideNavi
