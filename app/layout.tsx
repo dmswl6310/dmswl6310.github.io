9fce3a717e8c037bdc081e1860f7bd425226f76d
dmswl6310 <dmswl6310@gmail.com>
2026-06-24 16:03:08 +0900

    style(font): Pretendard 폰트 적용
    사이트 전체에서 Pretendard Variable을 우선 사용하도록 전역 폰트를 설정한다.
    
    배포 환경에서도 같은 글꼴이 보이도록 Pretendard 패키지를 의존성에 추가한다.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Link from "next/link";
+import "pretendard/dist/web/variable/pretendardvariable.css";
 import "./globals.css";
 
 export const metadata: Metadata = {
